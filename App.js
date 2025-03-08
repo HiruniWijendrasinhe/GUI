39189a3dfb52e99e3784be8d333369a814e9035a
A.D.H.L. Wijendrasinhe <lakshikahiruni20@gmail.com>
2025-03-09 02:27:37 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+
+
+
+import React from "react";
+import ToDoList from "./Index1234.js"; 
+import "./Newform/newform2.css";
+ 
+
+
+function App() {
+  return (
+    <div>
+      
+      <ToDoList/> {}
+      
+    
+    </div>
+    
+  );
+}
+
+export default App;
